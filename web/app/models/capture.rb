4fa87705ecd805bb4a294dcf2482652505d91d87
Andrew Stevenson <drew@barcodeblue.net>
2025-02-01 12:52:07 -0600

    add captures

---
+++ web/app/models/capture.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: captures
+#
+#  created_at :datetime         not null
+#  id         :integer          not null, primary key
+#  summary    :text
+#  title      :string
+#  updated_at :datetime         not null
+#  url        :string
+#
+
+class Capture < ApplicationRecord
+end
