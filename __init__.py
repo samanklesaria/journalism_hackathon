a6f6f1c405b2568f878d717f7ebc8bc99737f891
Sam Anklesaria <anklesarian@gmail.com>
2025-02-01 11:07:16 -0600

    Add docstring

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 def translate(audio_file):
+    """Returns a list of dicts with keys 'timestamp' and 'text'."""
     asr_pipeline = pipeline(
         "automatic-speech-recognition",
         model="openai/whisper-base",
     )
     results = asr_pipeline(
-        long_audio,
+        audio_file,
         max_new_tokens=256,
         generate_kwargs={"task": "transcribe"},
         return_timestamps=True,
         chunk_length_s=30,
         batch_size=8,
     )
+    return results['chunks']
